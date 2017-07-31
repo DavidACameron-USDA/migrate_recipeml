468bd5d503721fbf630ebb526d97da15a598e27c
David Cameron <david@cadeyrn.us>
2017-07-31 18:02:54 -0500

    Added RecipeML copyrights to plugin files.

--- src/Plugin/migrate/source/RecipeMLBase.php
+++ src/Plugin/migrate/source/RecipeMLBase.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * @file
+ * Copyright (c) FormatData. All rights reserved.
+ *
+ * Distribution of RecipeML Processing Software in source and/or binary forms is
+ * permitted provided that the following conditions are met:
+ * - Distributions in source code must retain the above copyright notice and
+ *   this list of conditions.
+ * - Distributions in binary form must reproduce the above copyright notice and
+ *   this list of conditions in the documentation and/or other materials
+ *   provided with the distribution.
+ * - All advertising materials and documentation for RecipeML Processing
+ *   Software must display the following acknowledgment:
+ *   "This product is RecipeML compatible."
+ * - Names associated with RecipeML or FormatData must not be used to endorse or
+ *   promote RecipeML Processing Software without prior written permission from
+ *   FormatData. For written permission, please contact RecipeML@formatdata.com.
+ */
+
 namespace Drupal\migrate_recipeml\Plugin\migrate\source;
 
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
